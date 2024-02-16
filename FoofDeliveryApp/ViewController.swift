208d6a6798279ddf4adb2f47510475af0c496607
Aliaksandr Dvoineu <av.dvoinev@gmail.com>
2024-02-16 17:44:39 +0300

    Initial Commit

---
+++ FoofDeliveryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoofDeliveryApp
+//
+//  Created by Aliaksandr Dvoineu on 16.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
